3b9e8f39cb02a94ebfd4c9cf8eff5633e0f25046
birdhale <110745846+birdhale@users.noreply.github.com>
2022-12-27 20:34:27 -0300

    News and Vocabulary Web-Scrapping

---
+++ italiannew.py
@@ -0,0 +1,72 @@
+import requests
+from bs4 import BeautifulSoup
+from docx import Document
+import random
+import openpyxl
+
+# Set the URL for the news website and send a request to the website
+
+URL = "https://easyitaliannews.com/"
+page = requests.get(URL)
+
+# Parse the HTML content of the page
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+# Find all the paragraphs on the website
+
+paragraphs = soup.find_all("p")
+
+# Select a random paragraph
+
+if paragraphs:
+    selected_paragraph = random.choice(paragraphs)
+else:
+    selected_paragraph = "Unable to find any paragraphs on the website"
+
+# Open the Word document
+
+document = Document("random_paragraph.docx")
+
+# Add the selected paragraph to the document
+
+document.add_paragraph(selected_paragraph.text)
+
+# Extract all the words from the document
+
+words = []
+for paragraph in document.paragraphs:
+    for run in paragraph.runs:
+        words.extend(run.text.split())
+
+# Select 10 random words from the list of words
+
+if len(words) >= 10:
+    selected_words = random.sample(words, 10)
+else:
+    selected_words = words
+
+# Open the Excel file
+
+workbook = openpyxl.load_workbook("existing_file.xlsx")
+
+# Get the first sheet in the workbook
+
+sheet = workbook.active
+
+# Find the next available row in the sheet
+
+next_row = sheet.max_row + 1
+
+# Paste the selected words into the Excel file, with each word in a different row
+
+for i, word in enumerate(selected_words):
+    sheet.cell(row=next_row + i, column=1).value = word
+
+# Save the Excel file
+
+workbook.save("existing_file.xlsx")
+
+# Save the Word document
+
+document.save("random_paragraph.docx")
